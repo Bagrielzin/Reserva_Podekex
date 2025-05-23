0d85847f5df8636e7ad906ff22cdae3b88754981
Bagrielzin <89031177+Bagrielzin@users.noreply.github.com>
2025-05-23 14:08:00 -0300

    Add files via upload

---
+++ backend/app/models/pokemon_model.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+from typing import List
+
+class Pokemon(BaseModel):
+    id: int
+    original_name: str
+    name: str
+    hp: int
+    skill: str
+    description: str
+    type: List[str]
+    viewed: bool
+    on_deck: bool
